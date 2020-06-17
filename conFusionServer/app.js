92aaa9176aebcfbf73dcdec2d4f59de919b0b30a
banura96 <mabhettiarachchi@gmail.com>
2020-06-17 22:51:15 +0530

    add new routers for mongoose models

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -30,7 +30,7 @@ app.use('/leaders',leaderRouter);
 
 const mongoose =require('mongoose');
 
-const Dishes = require('./models/dishes');
+ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
